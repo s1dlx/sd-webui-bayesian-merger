db6a2d831bac61a90768cb2620cf29b2aa252db8
s1dlx <s1dlx@proton.me>
2023-03-04 16:14:00 +0000

    mockup merge

--- README.md
+++ README.md
@@ -2,19 +2,37 @@
 
 ## What is this?
 
-...
+An opinionated take on stable-diffusion models-merging automatic-optimisation.
+
+... refs on bayesian optimisation ...
+
+Why opinionated? Because we use webui API and lots of config files to run the show. No GUI. 
+Embrace your inner touch-typist and leave the browser for the CLI.
 
 ## Requirements
 
-- [https://github.com/bbc-mc/sdweb-merge-block-weighted-gui](https://github.com/bbc-mc/sdweb-merge-block-weighted-gui)
+- A working installation of [stable-diffusion-webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui)
+- Installed [sdweb-merge-block-weighted-gui](https://github.com/bbc-mc/sdweb-merge-block-weighted-gui) extension
+
+## How to use
+
+- Start webui in `--api` mode
+- ...
 
 ## Stuff to add
 
 - [ ] wildcards
-
+- [ ] other prompt manipulation extensions
+- [ ] orthogonal sampler
+- [ ] UNET visualisation
 
 ## Stuff to test
 
-- Are existing scoring networks tuned towards photorealism?
-- Can we optimize for prompting style? Are wildcards enough?
-- Can we train a scoring network on personal preferences? How many images do we need? Tooling?
+- [ ] Are existing scoring networks tuned towards photorealism?
+- [ ] Can we optimize for prompting style? Are wildcards enough?
+- [ ] Can we train a scoring network on personal preferences? How many images do we need? Tooling?
+- [ ] Shall we use [sd-webui-supermerger](https://github.com/hako-mikan/sd-webui-supermerger) instead?
+
+## Inspired by
+
+- [sdweb-auto-MBW](https://github.com/Xerxemi/sdweb-auto-MBW)
--- sd-webui-bayesian-merger/merger.py
+++ sd-webui-bayesian-merger/merger.py
@@ -5,7 +5,24 @@ from bayes_opt import BayesianOptimization
 from generator import Generator
 from scorer import Scorer
 
+# TODO
 # from ../sd-webui-block-merge/scripts/merge import merge
+
+
+def merge(
+    weights,
+    model_a,
+    model_b,
+    device,
+    base_alpha,
+    output_file,
+    allow_overwrite,
+    verbose,
+    save_as_saferensors,
+    save_as_half,
+    skip_position_ids,
+):
+    pass
 
 
 class Merger:
@@ -29,6 +46,7 @@ class Merger:
         weights: list,
         base_alpha=0.5,
     ):
+        # TODO
         merge(
             weights,
             self.model_a,
@@ -64,13 +82,16 @@ class BayesianOptimisationMerger:
         # skip_position_ids = 0
 
         # TODO: weights and base_alpha from params
-        weights, base_alpha = params
+        # is the list of floats OK?
+        weights = [params[f"block_{i}"] for i in range(25)]
+        base_alpha = params['base_alpha']
 
         self.merger.sd_merge(
             weights,
             base_alpha,
         )
 
+        # TODO: is this forcing the model load despite the same name?
         self.generator.switch_model(self.output_file)
 
         # generate images
@@ -81,6 +102,7 @@ class BayesianOptimisationMerger:
         # score images
         scores = self.scorer.batch_score(images)
 
+        # spit out a single value for optimisation
         return self.scorer.average_score(scores)
 
     def optimize(self, payloads, init_points, n_iter):
@@ -90,11 +112,12 @@ class BayesianOptimisationMerger:
             payloads,
         )
 
-        # TODO: init UNET blocks
-        pbounds = {}
+        pbounds = {f"block_{i}": (0.0, 1.0) for i in range(25)}
+        pbounds['base_alpha': (0.0, 1.0)]
 
         # TODO: what if we want to optimise only certain blocks?
 
+        # TODO: fork bayesian-optimisation and add LHS
         self.optimizer = BayesianOptimization(
             f=partial_sd_target_function,
             pbounds=pbounds,
