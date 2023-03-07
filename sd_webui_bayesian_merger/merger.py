2db896daeef74c5581dcff83fb6cb0201123bd3c
s1dlx <s1dlx@proton.me>
2023-03-07 20:59:56 +0000

    and again

--- sd_webui_bayesian_merger/merger.py
+++ sd_webui_bayesian_merger/merger.py
@@ -37,6 +37,7 @@ class Merger:
     ):
         self.model_a = model_a
         self.model_b = model_b
+        self.output_file = None
         self.device = device
 
         # TODO: add as parameter?
@@ -58,7 +59,7 @@ class Merger:
             model_b_name = self.model_b.stem
             self.model_out_name = (f"bbwm-{model_a_name}-{model_b_name}.safetensors",)
             self.output_file = Path(
-                self.mode_a.parent, "f{self.model_out_name}.safetensors"
+                self.model_a.parent, "f{self.model_out_name}.safetensors"
             )
 
         re_inp = re.compile(r"\.input_blocks\.(\d+)\.")  # 12
