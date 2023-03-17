6bbebf15ba3ccc4fc17c7beb7fb2070c704c837c
Jonathan Mackenzie <jonmac1@gmail.com>
2023-03-17 11:05:36 +1100

    change loss to inverse

--- sd_webui_bayesian_merger/tpe_optimiser.py
+++ sd_webui_bayesian_merger/tpe_optimiser.py
@@ -12,7 +12,7 @@ class TPEOptimiser(Optimiser):
     def _target_function(self, params):
         res = self.sd_target_function(**params)
         return {
-            'loss': -res,
+            'loss': 1./(res + 1e-10),
             'status': STATUS_OK,
             'params': params
         }
