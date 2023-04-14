754e246886d5d23c58f7b905042aa9c824ae1ec8
s1dlx <s1dlx@proton.me>
2023-04-14 08:27:19 +0100

    merge onto theta_0

--- sd_webui_bayesian_merger/merger.py
+++ sd_webui_bayesian_merger/merger.py
@@ -1,11 +1,9 @@
-# adapted from
-# bbc-mc/sdweb-merge-block-weighted-gui/scripts/mbw/merge_block_weighted.py
-
 import os
 import re
+import sys
 
 from dataclasses import dataclass
-from typing import List, Dict, Tuple
+from typing import Dict, Tuple
 from pathlib import Path
 
 import torch
@@ -34,24 +32,102 @@ KEY_POSITION_IDS = ".".join(
     ]
 )
 
+NUM_MODELS_NEEDED = {
+    "add_difference": 3,
+    "weighted_add_difference": 3,
+    "weighted_sum": 2,
+    "sum_twice": 3,
+    "triple_sum": 3,
+    "weighted_double_difference": 5,
+}
+
+NAI_KEYS = {
+    "cond_stage_model.transformer.embeddings.": "cond_stage_model.transformer.text_model.embeddings.",
+    "cond_stage_model.transformer.encoder.": "cond_stage_model.transformer.text_model.encoder.",
+    "cond_stage_model.transformer.final_layer_norm.": "cond_stage_model.transformer.text_model.final_layer_norm.",
+}
+
+
+def fix_clip(model: Dict) -> Dict:
+    if KEY_POSITION_IDS in model:
+        model[KEY_POSITION_IDS] = torch.tensor(
+            [list(range(MAX_TOKENS))], dtype=torch.int64
+        )
+
+    return model
+
+
+def fix_key(model: Dict, key: str) -> Dict:
+    for nk in NAI_KEYS:
+        if key.startswith(nk):
+            model[key.replace(nk, NAI_KEYS[nk])] = model[key]
+            del model[key]
+
+    return model
+
+
+def fix_nai_keys(model: Dict) -> Dict:
+    for k in model:
+        model = fix_key(model, k)
+
+    return model
+
+
+# https://github.com/j4ded/sdweb-merge-block-weighted-gui/blob/master/scripts/mbw/merge_block_weighted.py#L115
+def fix_model(model: Dict) -> Dict:
+    model = fix_nai_keys(model)
+    return fix_clip(model)
+
 
 @dataclass
 class Merger:
     cfg: DictConfig
 
     def __post_init__(self):
-        self.model_a = Path(self.cfg.model_a)
-        self.model_b = Path(self.cfg.model_b)
-        self.model_c = Path(self.cfg.model_c)
-        if not self.model_c.exists():
-            self.model_c = None
-            self.model_name_suffix = f"bbwm-{self.model_a.stem}-{self.model_b.stem}"
-        else:
-            self.model_name_suffix = (
-                f"bbwm-{self.model_a.stem}-{self.model_b.stem}-{self.model_c.stem}"
-            )
+        self.parse_models()
         self.create_model_out_name(0)
         self.create_best_model_out_name()
+
+    def parse_models(self):
+        self.model_a = Path(self.cfg.model_a)
+        self.model_b = Path(self.cfg.model_b)
+        self.models = {"model_a": self.model_a, "model_b": self.model_b}
+        self.model_keys = ["model_a", "model_b"]
+        self.model_real_names = [
+            self.models["model_a"].stem,
+            self.models["model_b"].stem,
+        ]
+        self.greek_letters = ["alpha"]
+        seen_models = 2
+        for m in ["model_c", "model_d", "model_e"]:
+            if seen_models == NUM_MODELS_NEEDED[self.cfg.merge_mode]:
+                break
+            if m in self.cfg:
+                p = Path(self.cfg[m])
+            else:
+                break
+            if p.exists():
+                self.models[m] = p
+                self.model_keys.append(m)
+                self.model_real_names.append(self.models[m].stem)
+            else:
+                break
+            seen_models += 1
+        if self.cfg.merge_mode in [
+            "weighted_double_difference",
+            "sum_twice",
+            "triple_sum",
+        ]:
+            self.greek_letters.append("beta")
+        self.model_name_suffix = f"bbwm-{'-'.join(self.model_real_names)}"
+
+        try:
+            assert len(self.model_keys) == NUM_MODELS_NEEDED[self.cfg.merge_mode]
+        except AssertionError:
+            print(
+                "number of models defined not compatible with merge mode, aborting optimisation"
+            )
+            sys.exit()
 
     def create_model_out_name(self, it: int = 0) -> None:
         model_out_name = self.model_name_suffix
@@ -85,29 +161,36 @@ class Merger:
     def merge_key(
         self,
         key: str,
-        weights_alpha: List[float],
-        weights_beta: List[float],
-        base_alpha: float,
-        base_beta: float,
+        thetas: Dict,
+        weights: Dict,
+        bases: Dict,
         best: bool,
-        theta_0: Dict,
-        theta_1: Dict,
-        theta_2: Dict,
     ) -> Tuple[str, Dict]:
-        if "model" not in key or key not in theta_1:
-            return
-        if theta_2 and key not in theta_2:
-            return
         if KEY_POSITION_IDS in key:
-            return 
-
-        re_inp = re.compile(r"\.input_blocks\.(\d+)\.")  # 12
-        re_mid = re.compile(r"\.middle_block\.(\d+)\.")  # 1
-        re_out = re.compile(r"\.output_blocks\.(\d+)\.")  # 12
-        c_alpha = base_alpha
-        c_beta = base_beta
+            if self.cfg.skip_position_ids == 1:
+                if not best or self.cfg.best_precision == "16":
+                    return (key, thetas["model_a"][key].half())
+                return (
+                    key,
+                    thetas["model_a"][key],
+                )  # Skip position_ids key to eject effect. Value of Model A used.
+            elif self.cfg.skip_position_ids == 2:
+                thetas["model_a"][key] = torch.tensor(
+                    [list(range(MAX_TOKENS))], dtype=torch.int64
+                )
+                if not best or self.cfg.best_precision == "16":
+                    return (key, thetas["model_a"][key].half())
+                return (key, thetas["model_a"][key])
+        for theta in thetas.values():
+            if key not in theta:
+                return
+        current_bases = bases
         if "model.diffusion_model." in key:
             weight_index = -1
+
+            re_inp = re.compile(r"\.input_blocks\.(\d+)\.")  # 12
+            re_mid = re.compile(r"\.middle_block\.(\d+)\.")  # 1
+            re_out = re.compile(r"\.output_blocks\.(\d+)\.")  # 12
 
             if "time_embed" in key:
                 weight_index = 0  # before input blocks
@@ -124,97 +207,90 @@ class Merger:
                 raise ValueError(f"illegal block index {key}")
 
             if weight_index >= 0:
-                c_alpha = weights_alpha[weight_index]
-                if weights_beta:
-                    c_beta = weights_beta[weight_index]
+                current_bases = {k: w[weight_index] for k, w in weights.items()}
 
-        merged = self.merge_block(
-            c_alpha,
-            c_beta,
-            theta_0[key],
-            theta_1[key],
-            theta_2[key] if self.cfg.merge_mode != "weighted_sum" else None,
-            key in theta_2 if self.cfg.merge_mode != "weighted_sum" else None,
-        )
+        merged = self.merge_block(current_bases, thetas, key)
 
         if not best or self.cfg.best_precision == "16":
             merged = merged.half()
 
         return (key, merged)
 
-    def merge_block(
-        self,
-        alpha: float,
-        beta: float,
-        t0: Dict,
-        t1: Dict,
-        t2: Dict,
-        k_in_t2: bool,
-    ) -> Dict:
+    def merge_block(self, current_bases: Dict, thetas: Dict, key: str) -> Dict:
+        t0 = thetas["model_a"][key]
+        t1 = thetas["model_b"][key]
+        alpha = current_bases["alpha"]
+        if self.cfg.merge_mode == "weighted_sum":
+            return (1 - alpha) * t0 + alpha * t1
+
+        t2 = thetas["model_c"][key]
         if self.cfg.merge_mode == "add_difference":
-            return t0 + alpha * (t1 - t2) if k_in_t2 else t0
-        elif self.cfg.merge_mode == "sum_twice":
-            merged = (1 - alpha) * t0 + alpha * t1
-            return (1 - beta) * merged + beta * t2
+            return t0 + alpha * (t1 - t2)
+        elif self.cfg.merge_mode == "weighted_add_difference":
+            return (1 - alpha) * t0 + alpha * (t1 - t2)
+
+        beta = current_bases["beta"]
+        if self.cfg.merge_mode == "sum_twice":
+            return (1 - beta) * ((1 - alpha) * t0 + alpha * t1) + beta * t2
         elif self.cfg.merge_mode == "triple_sum":
             return (1 - alpha - beta) * t0 + alpha * t1 + beta * t2
-        elif self.cfg.merge_mode == "weighted_sum":
-            return (1 - alpha) * t0 + alpha * t1
+        elif self.cfg.merge_mode == "weighted_double_difference":
+            t3 = thetas["model_d"][key]
+            t4 = thetas["model_e"][key]
+            return (1 - alpha) * t0 + alpha * (
+                (1 - beta) * (t1 - t2) + beta * (t3 - t4)
+            )
 
     def merge(
         self,
-        weights_alpha: List[float],
-        weights_beta: List[float],
-        base_alpha: float,
-        base_beta: float,
+        weights: Dict,
+        bases: Dict,
         best: bool = False,
     ) -> None:
-        if len(weights_alpha) != NUM_TOTAL_BLOCKS:
-            raise ValueError(f"weights value must be {NUM_TOTAL_BLOCKS}")
+        thetas = {k: self.load_sd_model(m) for k, m in self.models.items()}
 
-        theta_0 = self.load_sd_model(self.model_a)
-        theta_1 = self.load_sd_model(self.model_b)
-        theta_2 = self.load_sd_model(self.model_c) if self.model_c else None
-        merged_model = {}
-        for key in tqdm(theta_0.keys(), desc="merging 1/1"):
+        for key in tqdm(thetas["model_a"].keys(), desc="stage 1"):
             if result := self.merge_key(
                 key,
-                weights_alpha,
-                weights_beta,
-                base_alpha,
-                base_beta,
+                thetas,
+                weights,
+                bases,
                 best,
-                theta_0,
-                theta_1,
-                theta_2,
             ):
-                merged_model[key] = result[1]
+                thetas["model_a"][key] = result[1]
 
-        for key in tqdm(theta_1.keys(), desc="merging 2/2"):
-            if "model" in key and key not in theta_0:
-                continue
-            if theta_2 and key not in theta_2:
-                continue
-            if KEY_POSITION_IDS in key:
-                continue
-            merged_model[key] = theta_1[key]
-            if not best or self.cfg.best_precision == "16":
-                merged_model[key] = merged_model[key].half()
+        for key in tqdm(thetas["model_b"].keys(), desc="stage 2"):
+            if "model" in key and key not in thetas["model_a"]:
+                if KEY_POSITION_IDS in key:
+                    if self.cfg.skip_position_ids == 1:
+                        continue
+                    elif self.cfg.skip_position_ids == 2:
+                        thetas["model_a"][key] = torch.tensor(
+                            [list(range(MAX_TOKENS))], dtype=torch.int64
+                        )
+                        if not best or self.cfg.best_precision == "16":
+                            thetas["model_a"][key] = thetas["model_a"][key].half()
+                        continue
+                thetas["model_a"].update({key: thetas["model_b"][key]})
+                if not best or self.cfg.best_precision == "16":
+                    thetas["model_a"][key] = thetas["model_a"][key].half()
+
+        thetas["model_a"] = fix_model(thetas["model_a"])
 
         if best:
             print(f"Saving {self.best_output_file}")
             if self.cfg.best_format == "safetensors":
                 safetensors.torch.save_file(
-                    merged_model,
+                    thetas["model_a"],
                     self.best_output_file,
                     metadata={"format": "pt"},
                 )
             else:
-                torch.save({"state_dict": merged_model}, self.best_output_file)
+                torch.save({"state_dict": thetas["model_a"]}, self.best_output_file)
         else:
             print(f"Saving {self.output_file}")
             safetensors.torch.save_file(
-                merged_model,
+                thetas["model_a"],
                 self.output_file,
                 metadata={"format": "pt", "precision": "fp16"},
             )
