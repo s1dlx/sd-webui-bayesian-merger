819b1b967177a73cb4a03cc483cf3d2b55911f95
s1dlx <s1dlx@proton.me>
2023-04-21 10:55:06 +0100

    fix paths

--- sd_webui_bayesian_merger/optimiser.py
+++ sd_webui_bayesian_merger/optimiser.py
@@ -206,14 +206,14 @@ class Optimiser:
 
         self.generator.switch_model(self.merger.model_out_name)
 
-        images, gen_paths = self.generate_images(paths, payloads)
+        images, gen_paths = self.generate_images()
         scores = self.score_images(images, gen_paths)
         avg_score = self.scorer.average_score(scores)
         self.update_best_score(bases, weights, avg_score)
 
         return avg_score
 
-    def generate_images(self, paths, payloads) -> Tuple[List, List]:
+    def generate_images(self) -> Tuple[List, List]:
         images = []
         gen_paths = []
         payloads, paths = self.prompter.render_payloads()
