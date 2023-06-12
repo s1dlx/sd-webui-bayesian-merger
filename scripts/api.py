781a01808a7256218f68a2393e8b6a32f4a16aeb
ljleb <set>
2023-06-12 02:14:37 -0400

    single shared memory, align memory

--- scripts/api.py
+++ scripts/api.py
@@ -2,7 +2,7 @@ from sd_webui_bayesian_merger.webui import load_model_weights
 from modules import script_callbacks, shared
 from multiprocessing import shared_memory
 from functools import reduce
-from typing import Optional
+from typing import Optional, Dict, Tuple, List
 import operator
 import fastapi
 import gradio as gr
@@ -12,29 +12,37 @@ import torch
 def on_app_started(_gui: Optional[gr.Blocks], api: fastapi.FastAPI):
     @api.post("/bbwm/load-shm-model")
     async def detect(
-        shapes: dict = fastapi.Body(title="serialized theta"),
+        model_shapes: Dict[str, Tuple[List[int], str]] = fastapi.Body(title="theta shapes by key"),
         model_a: str = fastapi.Body(None, title="path to reference checkpoint. used for VAE fallback and the like"),
     ):
         if model_a is None:
             model_a = shared.opts.data['sd_model_checkpoint']
 
-        key_memories = {}
         theta = {}
         try:
-            for k, (shape, dtype) in list(shapes.items()):
+            memory = shared_memory.SharedMemory(create=False, name=f"bbwm-model-bytes")
+            offset = 0
+            for k, (shape, dtype) in model_shapes.items():
                 dtype = getattr(torch, dtype)
-                key_memories[k] = shared_memory.SharedMemory(create=False, name=f"bbwm-{k}")
+                count = reduce(operator.mul, shape, 1)
                 theta[k] = torch.frombuffer(
-                    buffer=key_memories[k].buf,
-                    count=reduce(operator.mul, shape, 1),
+                    offset=offset,
+                    buffer=memory.buf,
+                    count=count,
                     dtype=dtype,
                 ).reshape(shape)
+                offset = align_offset(offset + count * theta[k].element_size())
 
             load_model_weights(theta, model_a)
 
         finally:
-            for mem in key_memories.values():
-                mem.close()
+            memory = locals().get('memory', None)
+            if memory is not None:
+                memory.close()
 
 
 script_callbacks.on_app_started(on_app_started)
+
+
+def align_offset(offset: int) -> int:
+    return offset + (8 - offset % 8) % 8
--- sd_webui_bayesian_merger/generator.py
+++ sd_webui_bayesian_merger/generator.py
@@ -37,17 +37,23 @@ class Generator:
         r.raise_for_status()
 
     def upload_model(self, theta: Dict, model_a: str) -> None:
-        memories = {}
-        payload_model = {}
+        shapes = {}
+        total_size = 0
+        for v in theta.values():
+            total_size = align_offset(total_size + v.untyped_storage().nbytes())
+
         try:
+            memory = shared_memory.SharedMemory(create=True, name=f"bbwm-model-bytes", size=total_size)
+            offset = 0
             for k, v in tqdm(list(theta.items()), desc="move model to shared memory"):
-                memories[k] = shared_memory.SharedMemory(create=True, name=f"bbwm-{k}", size=v.untyped_storage().nbytes())
-                memories[k].buf[:] = v.numpy(force=True).tobytes()
-                payload_model[k] = v.shape, str(v.dtype)[str(v.dtype).find('.') + 1:]
+                next_offset = offset + v.numpy().nbytes
+                memory.buf[offset:next_offset] = v.numpy(force=True).tobytes()
+                shapes[k] = v.shape, str(v.dtype)[str(v.dtype).find('.') + 1:]
                 del theta[k]
+                offset = align_offset(next_offset)
 
             option_payload = {
-                "model": payload_model,
+                "model_shapes": shapes,
                 "model_a": str(model_a),
             }
 
@@ -59,9 +65,10 @@ class Generator:
             r.raise_for_status()
 
         finally:
-            for mem in memories.values():
-                mem.close()
-                mem.unlink()
+            memory = locals().get('memory', None)
+            if memory is not None:
+                memory.close()
+                memory.unlink()
 
     def refresh_models(self) -> None:
         r = requests.post(url=f"{self.url}/sdapi/v1/refresh-checkpoints")
@@ -72,3 +79,7 @@ class Generator:
         r.raise_for_status()
 
         return [(m["title"], m["model_name"]) for m in r.json()]
+
+
+def align_offset(offset: int) -> int:
+    return offset + (8 - offset % 8) % 8
