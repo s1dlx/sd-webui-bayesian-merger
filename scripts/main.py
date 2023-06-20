9667816155a2c2ef6e6f99cdc88feaeca8f8fc89
ljleb <set>
2023-06-19 20:56:23 -0400

    buttons that never disable yet

--- scripts/main.py
+++ scripts/main.py
@@ -3,17 +3,38 @@ import pathlib
 import subprocess
 import sys
 from modules import script_callbacks
+from typing import Optional
 
 
-main_path = pathlib.Path(__file__).parent.parent / "bayesian_merger.py"
+MAIN_PATH = pathlib.Path(__file__).parent.parent / "bayesian_merger.py"
+TOTAL_MANUAL_SCORE_BUTTONS = 11
+pipe: Optional[subprocess.Popen] = None
 
 
 def on_ui_tabs():
     with gr.Blocks() as root:
-        generate_button = gr.Button(value="Optimize!", variant="primary")
+        generate_button = gr.Button(
+            value="Optimize!",
+            variant="primary",
+        )
+
+        score_buttons = []
+        with gr.Accordion(label="Manual scoring"):
+            with gr.Row():
+                for i in range(TOTAL_MANUAL_SCORE_BUTTONS):
+                    score_button = gr.Button(
+                        value=str(i),
+                        interactive=False,
+                    )
+                    score_button.click(
+                        fn=send_manual_score,
+                        inputs=[gr.State(i)],
+                    )
+                    score_buttons.append(score_button)
 
         generate_button.click(
-            fn=launch_optimizer
+            fn=launch_optimizer,
+            outputs=score_buttons
         )
 
     return [(root, "Bayesian Merger", "bbwm")]
@@ -23,12 +44,26 @@ script_callbacks.on_ui_tabs(on_ui_tabs)
 
 
 def launch_optimizer():
+    global pipe
     args = [
         sys.executable,
-        str(main_path),
+        str(MAIN_PATH),
+        #"--webui-protocol",
     ]
     print(f"Starting bayesian merger using command: {' '.join(args)}")
-    subprocess.Popen(
+    pipe = subprocess.Popen(
         args=args,
-        cwd=str(main_path.parent),
+        cwd=str(MAIN_PATH.parent),
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
     )
+
+    return [gr.Button.update(interactive=True)] * TOTAL_MANUAL_SCORE_BUTTONS
+
+
+def send_manual_score(score):
+    global pipe
+    stdout = pipe.communicate(input=str(score).encode('ascii'))
+    print(stdout[0].decode())
+    print(stdout[1].decode(), file=sys.stderr)
